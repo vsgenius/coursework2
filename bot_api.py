bec2be54a93be32e9ba5c78c221b4a3c42a9a290
Vladimir Belich <belichvladimir@mail.ru>
2022-04-11 09:37:34 +0300

    CW2

--- bot_api.py
+++ bot_api.py
@@ -7,12 +7,11 @@ import vk_api
 from vk_api.upload import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-TOKEN = '8830ad080fad0acf7aa774c028ea2bc7c528b8c1f99023903d72759b1a4ecbcafebbbb3c7da2d2c677db7'
-TOKEN2 = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-vk_group = vk_api.VkApi(token=TOKEN)
-vk_find = vk_api.VkApi(token=TOKEN2)
+vk_group = vk_api.VkApi(token=input('Please enter Token Group: '))
+vk_find = vk_api.VkApi(token=input('Please enter Token User: '))
 upload = VkUpload(vk_group)
 longpoll = VkLongPoll(vk_group)
+
 
 def send_msg(values) -> object:
     vk_group.method('messages.send', values)
@@ -70,10 +69,10 @@ def get_attachment_photo(list_files):
     return result
 
 
-def get_list_users(user_fields, user_id, peer_id,count_list_user=9):
+def get_list_users(user_fields, user_id, peer_id, count_list_user=9):
     count = 0
     for user in search_user(user_fields):
-        if not user['is_closed'] and check_db(user_fields['id'],user['id']):
+        if not user['is_closed'] and check_db(user_fields['id'], user['id']):
             count += 1
             list_files = get_user_photos(user['id'])
             send_msg({'user_id': user_id,
@@ -81,7 +80,7 @@ def get_list_users(user_fields, user_id, peer_id,count_list_user=9):
                       'peer_id': peer_id,
                       'message': f'{user["first_name"]} {user["last_name"]} https://vk.com/id{user["id"]}',
                       'attachment': get_attachment_photo(list_files)})
-            update_db(user_fields['id'],user['id'])
+            update_db(user_fields['id'], user['id'])
         if count == count_list_user: break
     if count == 0:
         send_msg({'user_id': user_id, 'random_id': 0, 'message': 'Никого не найдено'})
@@ -94,33 +93,35 @@ def get_user_field(user_id):
 
 
 def age_from(age_user, user_id):
-    age = age_user.split('.')
-    if len(age) < 3:
-        send_msg({'user_id': user_id, 'message': 'Возраст не указан. Укажите Нижний предел','random_id': randrange(10 ** 7)})
-        return int(wait_msg().text)-5
-    res = datetime.now().year - int(age[2])
+    if age_user is None or len(age_user.split('.')) < 3:
+        send_msg({'user_id': user_id, 'message': 'Возраст не указан. Укажите Нижний предел',
+                  'random_id': randrange(10 ** 7)})
+        return int(wait_msg().text)
+    res = datetime.now().year - int(age_user.split('.')[2])
     return res - 5
 
 
 def age_to(age_user, user_id):
-    age = age_user.split('.')
-    if len(age) < 3:
-        send_msg({'user_id': user_id, 'message': 'Возраст не указан. Укажите Верхний предел','random_id': randrange(10 ** 7)})
-        return int(wait_msg().text)+5
-    res = datetime.now().year - int(age[2])
+    if age_user is None or len(age_user.split('.')) < 3:
+        send_msg({'user_id': user_id, 'message': 'Возраст не указан. Укажите Верхний предел',
+                  'random_id': randrange(10 ** 7)})
+        return int(wait_msg().text)
+    res = datetime.now().year - int(age_user.split('.')[2])
     return res + 5
 
 
 def check_city(user_field, user_id):
     if user_field is None or user_field == '':
-        send_msg({'user_id': user_id, 'message': 'Город не заполнен. Укажите в ответном сообщении','random_id': randrange(10 ** 7)})
+        send_msg({'user_id': user_id, 'message': 'Город не заполнен. Укажите в ответном сообщении',
+                  'random_id': randrange(10 ** 7)})
         return wait_msg().text
     return user_field['id']
 
 
 def check_country(user_field, user_id):
     if user_field is None or user_field == '':
-        send_msg({'user_id': user_id, 'message': 'Страна не заполнена. Укажите в ответном сообщении','random_id': randrange(10 ** 7)})
+        send_msg({'user_id': user_id, 'message': 'Страна не заполнена. Укажите в ответном сообщении',
+                  'random_id': randrange(10 ** 7)})
         return wait_msg().text
     return user_field['id']
 
@@ -129,15 +130,16 @@ def search_user(user_fields):
     fields = 'age, sex, city, bdate,  hometown, is_closed'
     params = {'q': '', 'sort': 0, 'online': 1, 'has_photo': 1, 'count': 1000,
               'fields': fields,
-              'country': check_country(user_fields.get('country'), user_fields['id']),  # указать страна
               'status': user_fields.get('relation'),  # Женат не женат
               'age_from': age_from(user_fields.get('bdate'), user_fields['id']),  # возраст от
               'age_to': age_to(user_fields.get('bdate'), user_fields['id']),  # возраст до
-              'sex': (1 if user_fields.get('sex') == 2 else 2)}  # указать пол
+              'sex': (1 if user_fields.get('sex') == 2 else 2),
+              'country': check_country(user_fields.get('country'), user_fields['id'])}  # указать пол
     city = check_city(user_fields.get('city'), user_fields['id'])
-    if not city.isdigit():
-        params['hometown'] = city # указать город
-    else: params['city'] = user_fields['city']['id']
+    if not str(city).isdigit():
+        params['hometown'] = city  # указать город
+    else:
+        params['city'] = city
     user_lists = vk_find.method('users.search', params)
     return user_lists['items']
 
@@ -146,12 +148,13 @@ def bot_answer(event):
     request = event.text
     user_fields = get_user_field(event.user_id)
     if request.lower() == "привет" or request.lower() == 'hi':
-            send_msg({'user_id': user_fields['id'],
-                      'message': f"Добрый день {user_fields['first_name']},"
-                                 f"\nпо Вашим параметрам найдены следующие страницы:",
-                      'random_id': randrange(10 ** 7)})
-            get_list_users(user_fields, event.user_id, event.peer_id)
+        send_msg({'user_id': user_fields['id'],
+                  'message': f"Добрый день {user_fields['first_name']},"
+                             f"\nпо Вашим параметрам найдены следующие страницы:",
+                  'random_id': randrange(10 ** 7)})
+        get_list_users(user_fields, event.user_id, event.peer_id)
     elif request.lower() == "пока" or request.lower() == 'bye':
-        send_msg({'user_id': event.user_id, 'message': f"пока {user_fields['first_name']}",'random_id': randrange(10 ** 7)})
+        send_msg(
+            {'user_id': event.user_id, 'message': f"пока {user_fields['first_name']}", 'random_id': randrange(10 ** 7)})
     else:
-        send_msg({'user_id': event.user_id, 'message': 'Не поняла Вас....','random_id': randrange(10 ** 7)})
+        send_msg({'user_id': event.user_id, 'message': 'Не поняла Вас....', 'random_id': randrange(10 ** 7)})
--- bot_msg.py
+++
@@ -1,6 +0,0 @@
-from bot_api import get_list_users,get_user_field
-from random import randrange
-from main import vk_group, vk_find, longpoll
-from vk_api.longpoll import VkLongPoll, VkEventType
-
-
